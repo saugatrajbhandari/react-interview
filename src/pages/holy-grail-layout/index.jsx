28a68235d840e5a217c0e0fca66261421150b6fb
saugat rajbhandari <saugatrajbhandari47@gmail.com>
2025-01-13 12:53:43 +0545

    add holy grail layout

--- src/pages/holy-grail-layout/index.jsx
+++ src/pages/holy-grail-layout/index.jsx
@@ -1,7 +1,16 @@
+import style from "./holy-grail-layout.module.css";
 import React from "react";
 
 function HolyGrailLayout() {
-  return <div>HolyGrailLayout</div>;
+  return (
+    <div className={style.container}>
+      <div className={style.header}>this is header</div>
+      <div className={style.leftSidebar}>this is left sidebar</div>
+      <div className={style.main}>this is main sidebar</div>
+      <div className={style.rightSidebar}>this is right sidebar</div>
+      <div className={style.footer}>this is footer</div>
+    </div>
+  );
 }
 
 export default HolyGrailLayout;
